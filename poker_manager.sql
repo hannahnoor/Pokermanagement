0ade3edb387711e420346a1041019e13ac5214bf
Hannah <hannah.schonewille@student.nhlstenden.com>
2020-04-07 22:55:57 +0200

    Database aangepast

--- poker_manager.sql
+++ poker_manager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Gegenereerd op: 07 apr 2020 om 20:17
+-- Gegenereerd op: 07 apr 2020 om 20:54
 -- Serverversie: 8.0.19
 -- PHP-versie: 7.2.19
 
@@ -21,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `poker_manager`
 --
-CREATE DATABASE IF NOT EXISTS `poker_manager` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-USE `poker_manager`;
 
 -- --------------------------------------------------------
 
@@ -30,16 +28,12 @@ USE `poker_manager`;
 -- Tabelstructuur voor tabel `participant`
 --
 
-DROP TABLE IF EXISTS `participant`;
-CREATE TABLE IF NOT EXISTS `participant` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `participant` (
+  `id` int NOT NULL,
   `player_id` int NOT NULL,
   `table_id` int NOT NULL,
-  `rebuy_amount` int NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `participant_player` (`player_id`),
-  KEY `participant_table` (`table_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `rebuy_amount` int NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
 
@@ -47,16 +41,14 @@ CREATE TABLE IF NOT EXISTS `participant` (
 -- Tabelstructuur voor tabel `player_stats`
 --
 
-DROP TABLE IF EXISTS `player_stats`;
-CREATE TABLE IF NOT EXISTS `player_stats` (
+CREATE TABLE `player_stats` (
   `player_id` int NOT NULL,
   `round_won` int NOT NULL,
   `round_played` int NOT NULL,
   `tournament_won` int NOT NULL,
   `tournament_played` int NOT NULL,
-  `total_income` float NOT NULL,
-  PRIMARY KEY (`player_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `total_income` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
 
@@ -64,22 +56,15 @@ CREATE TABLE IF NOT EXISTS `player_stats` (
 -- Tabelstructuur voor tabel `round`
 --
 
-DROP TABLE IF EXISTS `round`;
-CREATE TABLE IF NOT EXISTS `round` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `round` (
+  `id` int NOT NULL,
   `table_id` int NOT NULL,
   `round_number` int NOT NULL,
   `small_blind` int NOT NULL,
   `big_blind` int NOT NULL,
   `dealer` int NOT NULL,
-  `player_won` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_round_participant` (`small_blind`),
-  KEY `FK_round_participant_2` (`big_blind`),
-  KEY `FK_round_participant_3` (`dealer`),
-  KEY `FK_round_participant_4` (`player_won`),
-  KEY `FK_round_table` (`table_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `player_won` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
 
@@ -87,11 +72,10 @@ CREATE TABLE IF NOT EXISTS `round` (
 -- Tabelstructuur voor tabel `tournament`
 --
 
-DROP TABLE IF EXISTS `tournament`;
-CREATE TABLE IF NOT EXISTS `tournament` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `game_code` varchar(255) NOT NULL,
-  `tournament_name` varchar(255) NOT NULL,
+CREATE TABLE `tournament` (
+  `id` int NOT NULL,
+  `game_code` varchar(255) COLLATE latin1_general_ci NOT NULL,
+  `tournament_name` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `admin` int NOT NULL,
   `start_amount` int NOT NULL,
   `max_rounds` int NOT NULL,
@@ -99,11 +83,8 @@ CREATE TABLE IF NOT EXISTS `tournament` (
   `chip_red` int NOT NULL,
   `chip_green` int NOT NULL,
   `chip_blue` int NOT NULL,
-  `chip_black` int NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `game_code` (`game_code`),
-  KEY `tournament_admin` (`admin`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `chip_black` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
 
@@ -111,13 +92,10 @@ CREATE TABLE IF NOT EXISTS `tournament` (
 -- Tabelstructuur voor tabel `tournament_table`
 --
 
-DROP TABLE IF EXISTS `tournament_table`;
-CREATE TABLE IF NOT EXISTS `tournament_table` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `tournament_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `table_tournament` (`tournament_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `tournament_table` (
+  `id` int NOT NULL,
+  `tournament_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
 
@@ -125,15 +103,97 @@ CREATE TABLE IF NOT EXISTS `tournament_table` (
 -- Tabelstructuur voor tabel `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `Email` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  `Name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `Email` (`Email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `Email` varchar(255) COLLATE latin1_general_ci NOT NULL,
+  `Password` varchar(255) COLLATE latin1_general_ci NOT NULL,
+  `Name` varchar(255) COLLATE latin1_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `participant`
+--
+ALTER TABLE `participant`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `participant_player` (`player_id`),
+  ADD KEY `participant_table` (`table_id`);
+
+--
+-- Indexen voor tabel `player_stats`
+--
+ALTER TABLE `player_stats`
+  ADD PRIMARY KEY (`player_id`);
+
+--
+-- Indexen voor tabel `round`
+--
+ALTER TABLE `round`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_round_participant` (`small_blind`),
+  ADD KEY `FK_round_participant_2` (`big_blind`),
+  ADD KEY `FK_round_participant_3` (`dealer`),
+  ADD KEY `FK_round_participant_4` (`player_won`),
+  ADD KEY `FK_round_table` (`table_id`);
+
+--
+-- Indexen voor tabel `tournament`
+--
+ALTER TABLE `tournament`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `game_code` (`game_code`),
+  ADD KEY `tournament_admin` (`admin`);
+
+--
+-- Indexen voor tabel `tournament_table`
+--
+ALTER TABLE `tournament_table`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `table_tournament` (`tournament_id`);
+
+--
+-- Indexen voor tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `participant`
+--
+ALTER TABLE `participant`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `round`
+--
+ALTER TABLE `round`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `tournament`
+--
+ALTER TABLE `tournament`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `tournament_table`
+--
+ALTER TABLE `tournament_table`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Beperkingen voor geëxporteerde tabellen
