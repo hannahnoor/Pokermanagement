54bb6bc7af4a5b23eeb85f42ff64011f973fa1c8
Hannah <hannah.schonewille@student.nhlstenden.com>
2020-04-13 15:04:20 +0200

    Pokertafel

--- speelscherm.php
+++ speelscherm.php
@@ -1,3 +1,17 @@
+<?php
+require_once 'checkLogin.php';
+
+// TODO: Kan deze beter in tournament?
+function calcLevel() {
+
+}
+
+function spreadPlayer() {
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="nl">
 <head>
@@ -37,6 +51,16 @@
 <body class="background">
 
   <div class="container centerScreen">
+      <div class="row">
+          <div class="col">
+              <div class="card">
+                  <div class="card-body">
+                      <h1 class="card-title">[Tafelniveau]</h1>
+                  </div>
+              </div>
+          </div>
+      </div>
+
     <div class="row">
       <div class="col">
         <div class="card">
@@ -49,42 +73,41 @@
                       <div class="modal-content">
                           <span class="close">&times;</span>
                           <pre>
-<b>Het doel van Poker hold’em</b>
-Het doel is om met de pocketkaarten en de deckkaarten een combinatie met een zo’n hoog mogelijke combinatie van maximaal vijf kaarten te maken.
+                            <b>Het doel van Poker hold’em</b>
+                            Het doel is om met de pocketkaarten en de deckkaarten een combinatie met een zo’n hoog mogelijke combinatie van maximaal vijf kaarten te maken.
 
-<b>Het spelverloop van poker hold’em</b>
-De small en big blind: degene die direct links van de dealer zit, moet een small blind kopen. Hoe hoog deze small blind is, wordt aan het begin van het spel bepaalt.
-De pokerspeler twee plekken naast de dealer is verplicht de big blind in te leggen. De overige spelers aan tafel hoeven niks in te leggen. Hierna begint het spel.
-De dealer deelt twee gesloten kaarten per persoon uit en legt drie gesloten kaarten op tafel neer.
+                            <b>Het spelverloop van poker hold’em</b>
+                            De small en big blind: degene die direct links van de dealer zit, moet een small blind kopen. Hoe hoog deze small blind is, wordt aan het begin van het spel bepaalt.
+                            De pokerspeler twee plekken naast de dealer is verplicht de big blind in te leggen. De overige spelers aan tafel hoeven niks in te leggen. Hierna begint het spel.
+                            De dealer deelt twee gesloten kaarten per persoon uit en legt drie gesloten kaarten op tafel neer.
 
-<b>De pre-flop:</b> iedereen mag zijn eigen kaarten bekijken. Op basis van deze kaarten kan er ingezet worden. De volgende acties zijn mogelijk: folden, checken of (re-)raisen.
-<b>De flop:</b> na de pre-flop ronde volgt de flop ronde; wat betekent dat de drie kaarten op tafel worden omgedraaid. De tweede inzetronde kan plaatsvinden en de mogelijkheden folden, checken of (re-)raisen zijn van toepassing.
-<b>De turn:</b> de vierde kaart op tafel wordt omgedraaid en kan opnieuw folden, checken of (re)-raisen)
-<b>De river:</b> de vijfde kaart wordt omgedraaid en de spelers aan tafel mogen voor de laatste keer inzetten (folden, checken en (re-) raisen.
-<b>De showdown:</b> de dealer vraagt iedereen om zijn of haar kaarten om te draaien en op basis van de hoogste kaartcombinatie wordt de winnaar bepaalt.
+                            <b>De pre-flop:</b> iedereen mag zijn eigen kaarten bekijken. Op basis van deze kaarten kan er ingezet worden. De volgende acties zijn mogelijk: folden, checken of (re-)raisen.
+                            <b>De flop:</b> na de pre-flop ronde volgt de flop ronde; wat betekent dat de drie kaarten op tafel worden omgedraaid. De tweede inzetronde kan plaatsvinden en de mogelijkheden folden, checken of (re-)raisen zijn van toepassing.
+                            <b>De turn:</b> de vierde kaart op tafel wordt omgedraaid en kan opnieuw folden, checken of (re)-raisen)
+                            <b>De river:</b> de vijfde kaart wordt omgedraaid en de spelers aan tafel mogen voor de laatste keer inzetten (folden, checken en (re-) raisen.
+                            <b>De showdown:</b> de dealer vraagt iedereen om zijn of haar kaarten om te draaien en op basis van de hoogste kaartcombinatie wordt de winnaar bepaalt.
 
-<b>De acties per speelronde Texas Hold’em</b>
-<b>Folden:</b> soms is het beter om je verliezen te nemen en eerder uit de ronde te stappen. In dit geval kun je kiezen voor folden; wat betekent dat je niet meer aan deze speelronde deelneemt.
-<b>Checken:</b> deze actie is alleen mogelijk wanneer de overige spelers de inzet niet verhoogt hebben. In dit geval kun je kiezen voor checken, wat betekent dat je niks inzet.
-<b>Raisen:</b> in dit geval verhoog je de inzet. Dit kun je doen omdat je goede kaarten hebt of omdat je wilt gaan bluffen.
-<b>Re-raisen:</b> wanneer je buurman zojuist voor raisen heeft gekozen, kun je er voor kiezen om te re-raisen. In dit geval verhoog je de inzet van je buurman.
+                            <b>De acties per speelronde Texas Hold’em</b>
+                            <b>Folden:</b> soms is het beter om je verliezen te nemen en eerder uit de ronde te stappen. In dit geval kun je kiezen voor folden; wat betekent dat je niet meer aan deze speelronde deelneemt.
+                            <b>Checken:</b> deze actie is alleen mogelijk wanneer de overige spelers de inzet niet verhoogt hebben. In dit geval kun je kiezen voor checken, wat betekent dat je niks inzet.
+                            <b>Raisen:</b> in dit geval verhoog je de inzet. Dit kun je doen omdat je goede kaarten hebt of omdat je wilt gaan bluffen.
+                            <b>Re-raisen:</b> wanneer je buurman zojuist voor raisen heeft gekozen, kun je er voor kiezen om te re-raisen. In dit geval verhoog je de inzet van je buurman.
 
-<b>Hoe win je in poker Hold’em?</b>
-Het doel van Texas Hold’em poker is om een zo’n hoog mogelijk combinatie met vijf kaarten te maken. Wanneer je de hoogste kaartencombinatie van alle overgebleven spelers aan tafel hebt, win je de speelronde.
+                            <b>Hoe win je in poker Hold’em?</b>
+                            Het doel van Texas Hold’em poker is om een zo’n hoog mogelijk combinatie met vijf kaarten te maken. Wanneer je de hoogste kaartencombinatie van alle overgebleven spelers aan tafel hebt, win je de speelronde.
 
-<b>De kaartencombinaties in poker Hold’em</b>
-Dit zijn de combinaties van kaarten die je kunt maken. Ze zijn hier op volgorde van beste naar slechtste.
-<b>Royal Flush:</b> aas, koning, vrouw, boer, 10 van gelijk teken is de hoogst mogelijke pokerhand.
-<b>Straight Flush:</b> Vijf opeenvolgende kaarten van gelijke teken. De aas kan zowel voor de twee als na de koning. Bij meerdere straight flush-handen beslist de hoogste kaart.
-<b>Four of a kind:</b> Vier gelijkwaardige kaarten in dit geval vier boeren. Een four of a kind-hand is in bijna alle gevallen de winnaar.
-<b>Full house:</b> Combinatie van three of a kind en een one pair. Soms komt er in dezelfde ronde twee of meer keer full house op tafel. Dan beslist de hoogste Three of a Kind, is deze gelijk dan is het pair beslissend.
-<b>Flush:</b> Vijf willekeurige kaarten van gelijke teken. Bij twee of meer flush-handen beslist de hoogste kaart, daarna de volgende in de reeks.
-<b>Straight:</b> Opeenvolgende kaarten van verschillende tekens. De aas kan voor de twee en na de koning. De laagst mogelijke straat is a-2-3-4-5, de hoogste is 10-j-v-k-a. Bij meerdere straight-handen beslist de hoogste kaart.
-<b>Three of a kind:</b> Drie gelijkwaardige kaarten, in dit geval drie koningen. Deze hand is vaak genoeg om de pot te winnen.
-<b>Two pair:</b> Verschillende paren. Bij meer two pair-handen is de waarde van het hoogste paar beslissend. Is die gelijk, dan beslist de waarde van het volgende paar of van de vijfde kaart.
-<b>One pair:</b> Soms is "een paartje" genoeg om de pot te winnen. Bij gelijke combinaties beslist de hoogte van de volgende kaart.
-<b>High card:</b> Vijf verschillende kaarten zonder een combinatie. Eerst kijk je natuurlijk wie de hoogste kaart heeft. Is die gelijk dan beslist de tweede kaart, is die ook gelijk dan gaat het om de waarde van de volgende.
-
+                            <b>De kaartencombinaties in poker Hold’em</b>
+                            Dit zijn de combinaties van kaarten die je kunt maken. Ze zijn hier op volgorde van beste naar slechtste.
+                            <b>Royal Flush:</b> aas, koning, vrouw, boer, 10 van gelijk teken is de hoogst mogelijke pokerhand.
+                            <b>Straight Flush:</b> Vijf opeenvolgende kaarten van gelijke teken. De aas kan zowel voor de twee als na de koning. Bij meerdere straight flush-handen beslist de hoogste kaart.
+                            <b>Four of a kind:</b> Vier gelijkwaardige kaarten in dit geval vier boeren. Een four of a kind-hand is in bijna alle gevallen de winnaar.
+                            <b>Full house:</b> Combinatie van three of a kind en een one pair. Soms komt er in dezelfde ronde twee of meer keer full house op tafel. Dan beslist de hoogste Three of a Kind, is deze gelijk dan is het pair beslissend.
+                            <b>Flush:</b> Vijf willekeurige kaarten van gelijke teken. Bij twee of meer flush-handen beslist de hoogste kaart, daarna de volgende in de reeks.
+                            <b>Straight:</b> Opeenvolgende kaarten van verschillende tekens. De aas kan voor de twee en na de koning. De laagst mogelijke straat is a-2-3-4-5, de hoogste is 10-j-v-k-a. Bij meerdere straight-handen beslist de hoogste kaart.
+                            <b>Three of a kind:</b> Drie gelijkwaardige kaarten, in dit geval drie koningen. Deze hand is vaak genoeg om de pot te winnen.
+                            <b>Two pair:</b> Verschillende paren. Bij meer two pair-handen is de waarde van het hoogste paar beslissend. Is die gelijk, dan beslist de waarde van het volgende paar of van de vijfde kaart.
+                            <b>One pair:</b> Soms is "een paartje" genoeg om de pot te winnen. Bij gelijke combinaties beslist de hoogte van de volgende kaart.
+                            <b>High card:</b> Vijf verschillende kaarten zonder een combinatie. Eerst kijk je natuurlijk wie de hoogste kaart heeft. Is die gelijk dan beslist de tweede kaart, is die ook gelijk dan gaat het om de waarde van de volgende.
                           </pre>
                       </div>
                   </div>
@@ -124,9 +147,79 @@ Dit zijn de combinaties van kaarten die je kunt maken. Ze zijn hier op volgorde 
       </div>
       <div class="col-9">
         <div class="card">
-          <div class="card-body">
-            <h1 class="card-title">POKERTAFEL</h1>
-            <div> <img src="https://previews.123rf.com/images/jsddesign/jsddesign1412/jsddesign141200170/34750183-realistic-poker-table.jpg" alt="poker table"></div>
+          <div class="card-body gameboard">
+              <div id="board-top">
+                  <div class="board-container">
+                      <div class="player">
+                          <i class="fas fa-user"></i>
+                          <p style="color: white;">Pipi</p>
+                      </div>
+                      <div class="player">
+                          <i class="fas fa-user"></i>
+                      </div>
+                      <div class="player">
+                          <i class="fas fa-user"></i>
+                      </div>
+                      <div class="player">
+                          <i class="fas fa-user"></i>
+                      </div>
+                      <div class="player">
+                          <i class="fas fa-user"></i>
+                      </div>
+
+                  </div>
+              </div>
+              <div id="board-bottom">
+                <div class="board-container">
+                    <div class="player">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="player">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="player">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="player">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="player">
+                        <i class="fas fa-user"></i>
+                    </div>
+                </div>
+              </div>
+
+            <!--  <div class="player" id="player1">
+                  <i class="fas fa-user"></i>
+              </div>
+             <div class="player" id="player2">
+                  <i class="fas fa-user"></i>
+              </div>
+             <div class="player" id="player3">
+                  <i class="fas fa-user"></i>
+              </div>
+              <div class="player" id="player4">
+                  <i class="fas fa-user"></i>
+              </div>
+              <div class="player" id="player5">
+                  <i class="fas fa-user"></i>
+              </div>
+              <div class="player" id="player6">
+                  <i class="fas fa-user"></i>
+              </div>
+              <div class="player" id="player7">
+                  <i class="fas fa-user"></i>
+              </div>
+              <div class="player" id="player8">
+                  <i class="fas fa-user"></i>
+              </div>
+              <div class="player" id="player9">
+                  <i class="fas fa-user"></i>
+              </div>
+              <div class="player" id="player10">
+                  <i class="fas fa-user"></i>
+              </div>
+              </div>-->
           </div>
         </div>
       </div>
